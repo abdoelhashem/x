44e6db5ae2b1dfa0c68dc0220f33a54985ae50f8
abdoahmed245 <amdmdmdm22@gmail.com>
2024-11-12 07:54:07 -0800

    Add

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/OneSignalSDKWorker.js', {
+    scope: '/',
+  }).then(function(registration) {
+    console.log('Service Worker registered with scope:', registration.scope);
+  }).catch(function(error) {
+    console.log('Service Worker registration failed:', error);
+  });
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
