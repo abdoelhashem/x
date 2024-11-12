d0a1162287705caeefdf2c05c130b444c60e5c58
abdoahmed245 <amdmdmdm22@gmail.com>
2024-11-12 08:43:18 -0800

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,29 @@ import Container from './container'
 import OneSignal from 'react-onesignal';
 function App() {
   useEffect(() => {
-    // تهيئة OneSignal
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-      OneSignal.init({
-        appId: 'ab1fe121-df44-4a43-a3b4-e112444f195a',  // استبدل بـ appId الخاص بك من لوحة تحكم OneSignal
-        allowLocalhostAsSecureOrigin: true, // يسمح للمطورين بتشغيل الإشعارات على localhost أثناء التطوير
+    // تأكد من تحميل OneSignal أولاً قبل استخدامه
+    if (window.OneSignal) {
+      // تهيئة OneSignal فقط بعد تحميل السكربت
+      window.OneSignal.push(function () {
+        window.OneSignal.init({
+          appId: 'ab1fe121-df44-4a43-a3b4-e112444f195a',  // استبدل بـ appId الخاص بك من لوحة تحكم OneSignal
+          allowLocalhostAsSecureOrigin: true, // يسمح للمطورين بتشغيل الإشعارات على localhost أثناء التطوير
+        });
+
+        // التعامل مع الاشتراك/الإلغاء
+        window.OneSignal.on('subscriptionChange', function (isSubscribed) {
+          if (isSubscribed) {
+            console.log('تم تفعيل الإشعارات!');
+          } else {
+            console.log('تم رفض الإشعارات!');
+          }
+        });
+
+        // إظهار المطالبة لتنشيط الإشعارات
+        window.OneSignal.showSlidedownPrompt();
       });
-    });
-
-    // التعامل مع الاشتراك/الإلغاء
-    OneSignal.push(function () {
-      OneSignal.on('subscriptionChange', function (isSubscribed) {
-        if (isSubscribed) {
-          console.log('تم تفعيل الإشعارات!');
-        } else {
-          console.log('تم رفض الإشعارات!');
-        }
-      });
-
-      // إظهار المطالبة لتنشيط الإشعارات
-      OneSignal.showSlidedownPrompt();
-    });
-
-  }, []);
+    }
+  }, []); // تأكد من أن التأثير يحدث مرة واحدة فقط عند تحميل الصفحة
   return (
     <div>
       <Container>
