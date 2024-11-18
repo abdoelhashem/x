4586ffa7dd8f090002c7f1aad83707cb0129f688
abdo ahmed <146590912+abdoelhashem@users.noreply.github.com>
2024-11-19 00:30:36 +0200

    Update messages.jsx

--- src/componants/messages.jsx
+++ src/componants/messages.jsx
@@ -45,7 +45,7 @@ function Messages() {
         
         // إنشاء تاريخ الرسالة في توقيت القاهرة
         const messageTime = new Date(`${date.month} ${date.day_of_month}, ${date.year} ${date.time_24hr}`);
-        messageTime.setHours(messageTime.getHours() + 1); // تقليل ساعة واحدة
+        messageTime.setHours(messageTime.getHours() + 2); // تقليل ساعة واحدة
         
         const elapsed = cairoTime - messageTime;
     
